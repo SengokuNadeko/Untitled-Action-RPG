7968b12907d8425fe5d2896844da9daf68059e3c
JihadBeydoun <hsun25874@gmail.com>
2023-03-14 12:44:41 -0400

    Added idle and walking animation blend trees, animations now play

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,7 +6,10 @@ public class PlayerManager : MonoBehaviour
 {
     private float movement_speed = 5;
     private Vector2 movement_direction;
+    private Vector2 last_movement_direction;
+   
     [SerializeField]private Rigidbody2D player_rigidbody;
+    [SerializeField]private Animator player_animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +21,30 @@ public class PlayerManager : MonoBehaviour
     {
         Inputs();
         Movement();
+        PlayWalkingAnimation();
     }
 
     void Inputs() 
     {
+        if (movement_direction.x != 0 || movement_direction.y != 0) 
+        {
+            last_movement_direction = movement_direction;
+        }
+
         movement_direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
     }
 
     void Movement() 
     {
         player_rigidbody.velocity = new Vector2(movement_direction.x * movement_speed, movement_direction.y * movement_speed);
+    }
+
+    void PlayWalkingAnimation()
+    {
+        player_animator.SetFloat("move_direction_x", movement_direction.x);
+        player_animator.SetFloat("move_direction_y", movement_direction.y);
+        player_animator.SetFloat("last_move_direction_x", last_movement_direction.x);
+        player_animator.SetFloat("last_move_direction_y", last_movement_direction.y);
+        player_animator.SetFloat("movement_magnitude", movement_direction.magnitude);
     }
 }
